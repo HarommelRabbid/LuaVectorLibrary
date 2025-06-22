83ae2eef896cab4d1d4fa93082aed1a2f512a10b
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-06-22 14:39:48 +0300

    added a simple example

---
+++ example.lua
@@ -0,0 +1,8 @@
+local vec2 = math.vector2(5, 0)
+local v1ec2 = math.vector2(0, 5)
+
+print("x of vec2: "..vec2:x(), "y of vec2"..vec2:y())
+print("x of v1ec2: "..v1ec2:x(), "y of v1ec2"..v1ec2:y())
+vec21 = vec2 + v1ec2
+
+print("x of vec21: "..vec21:x(), "y of vec21"..vec21:y())
